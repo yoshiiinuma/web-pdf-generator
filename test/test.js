5c55c489b6983a34d2db48fa8487c4e09c5c08a7
Iinuma <xiinuyo@ETSAdmins-MacBook-Air.local>
2018-09-03 20:39:43 -1000

    Set up base environment

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import chai from 'chai';
+import api from 'supertest';
+
+const expect = chai.expect;
+
+describe('Sample Test', function() {
+    it('returns true', function() {
+      expect(true).to.equal(true)
+    })
+})
+
